9caccde871dcd84517e78a4120cf1c24f239a140
Chloe Lam <chloelam2407@gmail.com>
2022-06-03 18:16:07 -0700

    timer

--- popup.js
+++ popup.js
@@ -1,8 +1,51 @@
+// checking Work Time >= Rest Time on button click
 
-function sayHi() {
-    return alert('you clicked me!');
+function checkTime() {
+    const workTime = document.getElementById('work-time').value;
+    const restTime = document.getElementById('rest-time').value;
+    console.log(workTime)
+    console.log(restTime)
+    console.log(workTime < restTime)
+
+    if (workTime < restTime) {
+        document.getElementById("warning1-text").style.display = "block";
+
+    }
+    // VALID
+    else {
+        document.getElementById("set-timer").style.display = "none";
+    } 
+    return 
 }
+document.getElementById('set-timer-button').addEventListener('click', checkTime);
 
-document.getElementById('button').addEventListener('click', sayHi);
+// TIMER
+
+function Timer(time) {
+    let minutes = time
+    let seconds = 0;
+  
+    const intervalId = setInterval(() => {
+      document.getElementById('timer').textContent = `${minutes < 1 ? 0 : ''}${minutes}:${seconds < 10 ? 0 : ''}${seconds}`;
+      if (seconds === 0 && minutes !== 0) {
+            seconds = 60;
+            --minutes;
+      }
+      --seconds;
+      if (seconds === 0 && minutes === 0) {
+        clearInterval(intervalId);
+        alert('your timer is done')
+      }
+    }, 1_000);
+};
+
+document.getElementById('start-timer-button').addEventListener('click', Timer(document.getElementById('work-time').value));
+document.getElementById('start-timer-button').innerHTML = 'Start Rest Timer';
+document.getElementById('start-timer-button').addEventListener('click', Timer(document.getElementById('rest-time').value));
+
+
+
+
+
 
 
